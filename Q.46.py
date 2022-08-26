84dfe29f666b338ef8baf249c5bad58fae8922a0
POOJABINJWA <110899989+POOJABINJWA@users.noreply.github.com>
2022-08-26 13:02:25 +0530

    Update Q.46.py

--- Q.46.py
+++ Q.46.py
@@ -6,13 +6,13 @@
 # Concatenate element-wise three said lists:
 # ['0red100', '1green200', '2black300', '3blue400', '4white500']
 
-# a=['0', '1', '2', '3', '4']
-# b=['red', 'green', 'black', 'blue', 'white']
-# c=['100', '200', '300', '400', '500']
-# i=0
-# e=[]
-# while i<len(a):
-#     e.append(a[i]+b[i]+c[i])
-#     i=i+1
-# print(e)
+a=['0', '1', '2', '3', '4']
+b=['red', 'green', 'black', 'blue', 'white']
+c=['100', '200', '300', '400', '500']
+i=0
+e=[]
+while i<len(a):
+    e.append(a[i]+b[i]+c[i])
+    i=i+1
+print(e)
 
