b7d522631a9e86f8c335b106e44947306becae77
POOJABINJWA <110899989+POOJABINJWA@users.noreply.github.com>
2022-08-26 13:04:07 +0530

    Update append two list .py

--- append two list .py
+++ append two list .py
@@ -1,29 +1,29 @@
-# a=[10,20,30,40]
-# b=[100,200,300,400]
-# i=0
-# j=1
-# c=[]
-# while i<len(a):
-#     d=a[i],b[-j]
-#     c.append(d)
-#     i=i+1
-#     j=i+1
-# print(c,end="")    
+a=[10,20,30,40]
+b=[100,200,300,400]
+i=0
+j=1
+c=[]
+while i<len(a):
+    d=a[i],b[-j]
+    c.append(d)
+    i=i+1
+    j=i+1
+print(c,end="")    
 
-# a=[2,2,2,3,1,4,4,5,6,7,5,8,9]
-# d=[]
-# i=0
-# while(i<len(a)):
-#     j=0
-#     c=0
-#     while(j<len(a)):
-#         if a[i]==a[j]:
-#             c=c+1
-#         j=j+1
-#     if c>1:
-#         d.append(a[i])
-#     i=i+1
-# print(d)
+a=[2,2,2,3,1,4,4,5,6,7,5,8,9]
+d=[]
+i=0
+while(i<len(a)):
+    j=0
+    c=0
+    while(j<len(a)):
+        if a[i]==a[j]:
+            c=c+1
+        j=j+1
+    if c>1:
+        d.append(a[i])
+    i=i+1
+print(d)
 
 
 a=[2,2,2,3,1,4,4,5,6,7,5,8,9]
@@ -40,15 +40,15 @@ while(i<len(a)):
         c=c+1
     i=i+1
 print(d)
-# i=0
-# while i(len(a)):
-#     j=0
-#     c=0
-#     while j<len(a[i]):
-#         if a[i]==a[j]:
-#             c=c+1
-#         j=j+1
-#     if c>1:
-#         d.append(a[i])
-#     i=i+1
-# print(d,c)
+i=0
+while i(len(a)):
+    j=0
+    c=0
+    while j<len(a[i]):
+        if a[i]==a[j]:
+            c=c+1
+        j=j+1
+    if c>1:
+        d.append(a[i])
+    i=i+1
+print(d,c)
